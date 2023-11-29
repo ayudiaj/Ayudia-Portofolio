1585169ec62f544a5b1d8f96a4c0189441b71656
AyudiaJayanti <ayudiajayanti05@gmail.com>
2023-11-29 18:00:49 +0700

    push to repo

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary' : '#3669C9',
+        'white' : '#FFFF',
+        'light-gray' : '#D1D5DB',
+        'gray' : '#808080',
+        'deep-gray' : '#0A0A0F',
+        'light-dark' : '#313131',
+        'dark' : '#101017',
+        'deep-dark' : '#050507',
+      }
+    },
+  },
+  plugins: [],
+} 
+
